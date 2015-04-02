65b44b7f344f1001b7fdad9d36f1ed011dc21f15
Timur <timur@RoadWarrior>
2015-04-01 21:48:54 -0400

    The first release of the graphing tool with ARMA feature.

--- AdvanceGrapher.py
+++ AdvanceGrapher.py
@@ -11,23 +11,41 @@ from matplotlib.backends.backend_gtk3 import NavigationToolbar2GTK3 as Navigatio
 import numpy as np
 from numpy import arange, sin, pi
 
+import statsmodels.api as sm
+
 import pandas as pd
 from pandas import DataFrame
 import pandas.io.data
 
 import datetime
 
+import re
 
+class PlotBox():
+    def __init__(self):
+        self.fig=Figure(figsize=(4,3), dpi=50)
+        self.canvas=FigureCanvas(self.fig)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.vbox=gtk.VBox()
+        self.vbox.pack_start(self.canvas, True, True, 0)
+        self.vbox.pack_start(self.toolbar, False, False, 0)
 
+class ProduceOutput():
+    def __init__(self):
+        self.ResultsTable=gtk.Table(3,6,True)
+        
+    def Label(self,txt):
+        return 
+        
 class AdvanceGrapher(gtk.Window):
     
     def __init__(self):
         gtk.Window.__init__(self,title="AdvanceGrapher")
         self.df=DataFrame
         
-        self.set_default_size(800, 800)
+        self.set_default_size(300, 600)
         
-        self.mainTable=gtk.Table(12,4,True)
+        self.mainTable=gtk.Table(16,8,True)
         self.add(self.mainTable)
         
         
@@ -39,8 +57,25 @@ class AdvanceGrapher(gtk.Window):
         
         self.btnPlotDependentVar=gtk.Button(label="Plot")
         self.btnPlotDependentVar.connect("clicked",self.btnPlotDependentVar_clicked)
+        self.btnAddToExog=gtk.Button(label="Add to Exog")
+        self.btnAddToExog.connect("clicked",self.btnAddToExog_clicked)
+        self.btnRemoveFromExog=gtk.Button(label="Remove from Exog")
+        self.btnRemoveFromExog.connect("clicked",self.btnRemoveFromExog_clicked)
         
-        
+        self.btnFitModel=gtk.Button(label="Fit Mofel")
+        self.btnFitModel.connect("clicked",self.btnFitModel_clicked)
+        self.entryOrderAR=gtk.Entry()
+        self.entryOrderMA=gtk.Entry()
+        self.mainTable.attach(gtk.Label("AR"),0,1,10,11)
+        self.mainTable.attach(gtk.Label("MA"),0,1,11,12)
+        self.mainTable.attach(self.entryOrderAR,1,2,10,11)
+        self.mainTable.attach(self.entryOrderMA,1,2,11,12)
+        self.mainTable.attach(gtk.Label("Cutoff Time"), 0,1,12,13)
+        self.CutoffTimeList=gtk.ListStore(int, str)
+        self.CutoffTimeCombo = gtk.ComboBox.new_with_model_and_entry(self.CutoffTimeList)
+        self.CutoffTimeCombo.set_entry_text_column(1)
+        self.mainTable.attach(self.btnFitModel,0,1,13,14)
+        self.mainTable.attach(self.CutoffTimeCombo,1,2,12,13)
         
         self.entryFileName=gtk.Entry()
         self.entryFileName.set_text("/home/timur/ProjectsLocal/Data/DFAST2015/DFAST2015base.csv")
@@ -52,45 +87,149 @@ class AdvanceGrapher(gtk.Window):
         self.ListOfVars.append([1,'hello'])
         self.ListOfVars.append([2,'world'])
         self.sampleTreeView=gtk.TreeView(self.ListOfVars)
-        #self.selector=self.sampleTreeView.get_selection()
-        #self.selector.set_mode(gtk.SelectionMode.MULTIPLE)
-        #self.sampleTreeview.insert_column('hello',1)
         
-        self.fig=Figure(figsize=(4,3), dpi=100)
-        self.canvas=FigureCanvas(self.fig)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.vbox=gtk.VBox()
-        self.vbox.pack_start(self.canvas, True, True, 0)
-        self.vbox.pack_start(self.toolbar, False, False, 0)
+        self.ListOfExogVars=gtk.ListStore(int, str)
+        self.ListOfExogVarsCombo = gtk.ComboBox.new_with_model_and_entry(self.ListOfExogVars)
+        self.ListOfExogVarsCombo.set_entry_text_column(1)
+        
+
+        self.pbox1=PlotBox()
+        self.pbox2=PlotBox()
         
         self.mainTable.attach(self.btnChooseFile,0,1,0,1)
         self.mainTable.attach(self.entryFileName,2,5,0,1)
         self.mainTable.attach(self.btnLoadData,1,2,0,1)
         
         self.mainTable.attach(gtk.Label("Dependent Var"), 0,1,1,2)
+        self.mainTable.attach(gtk.Label("Exogen Vars"), 0,1,4,5)
+        self.mainTable.attach(self.btnAddToExog,1,2,3,4)
+        self.mainTable.attach(self.ListOfExogVarsCombo, 1,2,4,5)
+        self.mainTable.attach(self.btnRemoveFromExog,1,2,5,6)
+        
+        
         self.mainTable.attach(self.DependentVarCombo, 1,2,1,2)
-        
         self.mainTable.attach(self.btnPlotDependentVar,1,2,2,3)
+        self.mainTable.attach(self.pbox1.vbox, 2,5,1,14)
+        self.mainTable.attach(self.pbox2.vbox, 5,8,7,14)
         
-        self.mainTable.attach(self.vbox, 2,6,1,8)
+
+        self.ResultsWindow=gtk.ScrolledWindow()
+        self.ResultsWindow.set_hexpand(True)
+        self.ResultsWindow.set_vexpand(True)
+        #self.ResultsLabel.set_justify(gtk.Justification.LEFT)
+        # self.ResultsLabel.set_margin_top(0)
+        #self.ResultsLabel.set_margin_left(0)
+        #print dir(self.ResultsLabel)
+        self.mainTable.attach(self.ResultsWindow,5,8,0,7)
         
-        self.mainTable.attach(self.sampleTreeView,1,2,3,8)
-        #return None
+        self.textview = gtk.TextView()
+        self.textbuffer = self.textview.get_buffer()
+        self.textbuffer.set_text("This is some text inside of a Gtk.TextView. "
+            + "Select text and click one of the buttons 'bold', 'italic', "
+            + "or 'underline' to modify the text accordingly.")
+        self.ResultsWindow.add(self.textview)
+        
+  
+        
+    def btnRemoveFromExog_clicked(self,widget):
+        activeIter=self.ListOfExogVarsCombo.get_active_iter()
+        model=self.ListOfExogVarsCombo.get_model()
+        self.ListOfExogVars.remove(activeIter)
+        return 0
+     
+    def btnAddToExog_clicked(self,widget):
+        #self.ListOfVars.append([i, self.df.columns[i]])
+        #self.ListOfExogVarsCombo
+        print dir(self.ListOfExogVars)
+        print len(self.ListOfExogVars)
+        print self.Variable
+        self.ListOfExogVars.append([len(self.ListOfExogVars),self.Variable])
+        return 0
+       
+    def btnFitModel_clicked(self,widget):
+        #model = sm.tsa.arima_model.ARMA(self.df[Variable], (2, 2)).fit(trend='nc', disp=0)
+        exogvars=[]
+        for i in range(0,len(self.ListOfExogVars)):
+            exogvars.append(self.ListOfExogVars[0].model[i][1])
+
+        activeIter=self.CutoffTimeCombo.get_active_iter()
+        cutoff=self.CutoffTimeCombo.get_model()
+        cutoff2=cutoff[activeIter][0]
+        print cutoff[activeIter][0]
+        
+        AR=int(self.entryOrderAR.get_text())
+        MA=int(self.entryOrderMA.get_text())
+        
+        #print self.df[exogvars]
+        
+        if len(exogvars)>0:
+            model=sm.tsa.ARMA(self.df[self.Variable][0:cutoff2],(AR,MA),exog=self.df[exogvars][0:cutoff2])
+            results=model.fit()
+            #oot=results.predict(start=cutoff2,exog=self.df[exogvars])
+            oot=results.forecast(steps=len(self.df)-cutoff2, exog=self.df[exogvars][cutoff2:len(self.df)])
+            #oot_series=pd.Series(oot[0],self.df.index[cutoff2:len(self.df)])
+            
+        else:
+            model=sm.tsa.ARMA(self.s[0:cutoff2],(AR,MA))
+            results=model.fit()
+            oot=results.forecast(steps=len(self.df)-cutoff2)
+            #print len(self.df)-cutoff2
+            #oot_series=pd.Series(oot[0],self.df.index[cutoff2:len(self.df)])
+            #self.df['forecast']=oot_series
+            #oot=results.predict(start=self.df.index[cutoff2-1])
+        
+        oot_95l=pd.Series(oot[2][:,0],self.df.index[cutoff2:len(self.df)])
+        oot_95u=pd.Series(oot[2][:,1],self.df.index[cutoff2:len(self.df)])
+        oot_series=pd.Series(oot[0],self.df.index[cutoff2:len(self.df)])
+        self.df['forecast']=oot_series
+        self.df['95l']=oot_95l
+        self.df['95u']=oot_95u
+        
+        #print str(results.summary())
+        #print type(str(results.summary()))
+        SummaryStr=str(results.summary2())
+        
+        #SummaryStr=re.sub('\n','',SummaryStr)
+        #self.ResultsLabel.set_text(SummaryStr)
+        self.textbuffer.set_text(SummaryStr)
+
+        self.pbox2.fig.clf(keep_observers=True)
+        a=self.pbox2.fig.add_subplot(111, title='Out of time Forecast')
+        a.plot(self.df.index,self.df[self.Variable],label='Actual',color="blue")
+        a.plot(self.df.index,self.df['forecast'],label='Forecast',color="red")
+        a.plot(self.df.index,self.df['95l'],label='95% Conf',color="green",linestyle="--")
+        a.plot(self.df.index,self.df['95u'],color="green",linestyle="--")
+        a.legend(loc="best")
+        self.pbox2.canvas.draw()
+        
+        print 'hello'
+        print SummaryStr
+        print 'world'
+        print results.params[1]
+        print results.pvalues
+        
+        return 0
     
     def btnPlotDependentVar_clicked(self,widget):
         activeIter=self.DependentVarCombo.get_active_iter()
         model=self.DependentVarCombo.get_model()
         Variable=model[activeIter][1]
-          
-        a=self.fig.add_subplot(111,title=Variable)
-        a.clear()
-        a=self.fig.add_subplot(111,title=Variable)
-        t = self.df[Variable]
-        s = self.df[Variable]
+        self.Variable=Variable
+        self.pbox1.fig.clf(keep_observers=True)
+        a=self.pbox1.fig.add_subplot(311,title=Variable)
+        
+        self.s = self.df[Variable]
         a.plot(self.df.index,self.df[Variable])
-        self.canvas.draw()
         
+        b=self.pbox1.fig.add_subplot(312)
+        sm.graphics.tsa.plot_acf(self.s,ax=b,lags=12,alpha=.05)
+        b=self.pbox1.fig.add_subplot(313)
+        sm.graphics.tsa.plot_pacf(self.s,ax=b,lags=12,alpha=.05)
+        self.pbox1.canvas.draw()
         
+        #acf=sm.tsa.stattools.acf(s,nlags=12, qstat=True, alpha=.05)
+        #pacf=sm.tsa.stattools.pacf(s,nlags=12)
+
         return 0
     
     def DependentVarCombo_changed(self, combo):
@@ -118,6 +257,11 @@ class AdvanceGrapher(gtk.Window):
         for i in range(0,len(self.df.columns)):
             self.ListOfVars.append([i, self.df.columns[i]])
             print self.df.columns[i]
+            
+        print self.df.index[3]
+        print len(self.df.index)
+        for i in range(0,len(self.df.index)):
+            self.CutoffTimeList.append([i,str(self.df.index[i])])
         return 0
     
     def add_filters(self, dialog):
---
+++ readme.txt
@@ -0,0 +1,14 @@
+The following python program "AdvanceGrapher.py" is a gui wrapper
+to help user view time series data, and fit ARMA models on the data.
+
+Requirements:
+numpy version 1.8.2
+scipy version 0.13.3
+pandas version 0.13.1
+statsmodels version 0.6.1
+
+The imported data needs to be in the csv format 
+Date,Var1,Var2
+03/31/2001,-1.1,1.4
+06/30/2001,2.1,5.1
+09/30/2001,-1.3,0.7
